ee6823619811530ce27245e00cbf14cdbc302b49
mohamed.dahani.dev <mohamed.dahani.dev@gmail.com>
2024-07-17 21:34:27 +0100

    add link github of snackly project

--- src/assets/data/Data.js
+++ src/assets/data/Data.js
@@ -12,7 +12,7 @@ const projectsData = [
         title: "Snackly",
         description:
           "Revolutionize online shopping with our React full-stack e-commerce website, seamlessly integrating frontend and backend technologies for a dynamic, user-friendly experience.",
-        gitHub: "",
+        gitHub: "https://github.com/mohamed-dahani-dev/frontend-snacly",
         link: "",
       },
   {
